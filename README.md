4228c22a746a98b15922c0d16e6e3077e27fc253
error666 <372706161@qq.com>
2017-06-19 17:16:34 +0800

    fix some bugs
    make MutationObserver monitor certain dom instand of whole document

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ a chrome extension demo for bilibili
 某日心态崩坏，在B站又是满屏幕不想看的视频，于是开始捣鼓。
 初版雏形。
 慢慢修bug中。
+
+<img src="images\preview.png"></img>
--- script.js
+++ script.js
@@ -8,6 +8,16 @@ chrome.runtime.sendMessage({
   keywords = str !== '' ? str.split(',') : [];
 });
 
+// keywords是异步获取的，可能运行至此，值还没有传过来，所以用延时来解决
+if (keywords.length === 0) {
+    setTimeout(function() {
+        processPage();
+    }, 1500);
+} else {
+    processPage();
+}
+
+
 // 当页面加载时，运行主要处理函数
 // 使用MutationObserver来检测页面的变动
 let MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
@@ -18,15 +28,16 @@ if (observerMutationSupport) {
       processPage();
     });
   });
-  const options = {
+
+var observeClass = $('[class*=clearfix],[class*=container-row],[class=container-body],[class*=rm-list-wrp],[class*=ajax-render]');
+const options = {
     "childList": true, //子节点的变动
-    "attributes": true, //属性的变动
     "characterData": true, //节点内容或节点文本的变动
     "subtree": true, //所有后代节点的变动
-    "attributeOldValue": true, //表示观察attributes变动时，是否需要记录变动前的属性
-    "characterDataOldValue": true //表示观察characterData变动时，是否需要记录变动前的值
   };
-  observer.observe(document, options);
+  for (var i = observeClass.length - 1; i >= 0; i--) {
+      observer.observe(observeClass[i], options);
+  }
 }
 
 /* 主要的处理函数 */
@@ -35,8 +46,9 @@ function processPage() {
   for (var i = 0; i < keywords.length; i++) {
     var keyword = keywords[i];
     if (keyword) {
+      // console.log(keyword);
       var dislikething = $("[title*='" + keyword + "'],[data-txt*='" + keyword + "'],[data-up*='" + keyword + "']");
-      // console.log(dislikething)
+      // console.log(dislikething);
       dislikething.each(function() {
         if ($(this).parents("li")[0]) {
           count += 1;
@@ -44,6 +56,7 @@ function processPage() {
             method: "sendDefenseCount",
             param: count
           });
+          console.log($(this).parents("li")[0]);
           $(this).parents("li")[0].remove();
         }
         if($(this).is("[class*='tag-item']")){
